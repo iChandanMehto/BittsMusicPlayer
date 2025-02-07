24244b0c35cf91695947262cc8c831f683ec630f
Chandan Mehto <mehtochandan78@gmail.com>
2025-02-07 15:15:13 +0530

    responsive changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,8 +27,8 @@ const Navbar = (prop) => {
   return (
 
 
-    <nav className="bg-slate-950 h-[75px] w-full fixed top-0   md:m-0 p-0  shadow-md items-center align-middle ">
-      <div className='no-underline bg-slate-950 flex flex-row justify-between '>
+    <nav className="bg-slate-950  mr-2 h-[75px] w-full fixed top-0   mb-5 rounded-b-2xl   md:rounded-none  md:m-0 p-0 border border-slate-900 shadow-md items-center align-middle ">
+      <div className='no-underline rounded-3xl bg-slate-950 flex flex-row justify-between '>
         <ul className='no-underline flex  bg-slate-950 text-white  justify-center md:justify-start gap-1   items-center mr-5 md:gap-10 list-none font-normal text-[19px] ml-[1%]  '>
           {/* <li className='text-white'> <a className='text-white' href="#">{prop.Home}</a>Home</li> */}
           <li className='   text-royalblue  ml-3 md:ml-1.5  md:pl-[7px] w-[40vw] md:w-[500px]  flex items-center text-[17px] bg-[#f5f5f5]  h-[37px] mb-[10px] mt-[20px] pr-[40px] rounded-full shadow-[0_0_8px_#ddd] border-none outline-none bg-no-repeat" style="background-position: right 7px; background-size: 25px; '>
@@ -36,10 +36,10 @@ const Navbar = (prop) => {
             <input className='focus:outline-none bg-transparent h-full border-none text-[1.10rem] w-full ml-[10px]  text-black' type="text" placeholder="Functionality coming soon! Stay tuned." ></input>
           </li>
         </ul>
-        <ul className="text-white flex flex-row gap-3  m-3 md:mr-3 items-center text-lg">
+        {/* <ul className="text-white flex flex-row gap-3  m-3 md:mr-3 items-center text-lg">
           <li><button>Sign in</button></li>
           <li className="bg-white text-black rounded-full md:py-1.5 md:px-4 md:font-bold font  shadow-[0_0_8px_#ddd] border-none outline-none bg-no-repeat px-1.5 py-1 "><button >Log In</button></li>
-        </ul>
+        </ul> */}
 
       </div>
 
